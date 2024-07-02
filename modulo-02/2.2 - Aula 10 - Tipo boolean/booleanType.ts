3f4d8a782537c2a797805cd1c04675c0e2db191e
hudney-fsbrito <hudney.rse@gmail.com>
2024-07-02 10:41:39 -0300

    Modulo 2 - Tipo boolean

---
+++ modulo-02/2.2 - Aula 10 - Tipo boolean/booleanType.ts
@@ -0,0 +1,32 @@
+//REFERÊNCIA
+// https://www.typescriptlang.org/docs/handbook/intro.html
+
+//Exemplo 01
+
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+
+//Exemplo 02
+
+let concluida: boolean = false;
+
+if (concluida) {
+  console.log('Esta tarefa foi concluída com sucesso!');
+  
+} else {
+  console.log('Tarefa Pendente!');
+  
+}
+
+//Exemplo de casa
+
+let idade: number = 8;
+ if (idade >= 18) {
+  console.log('Pessoa maior de idade');
+ } else {
+  console.log('Menor de idade');
+  
+ }

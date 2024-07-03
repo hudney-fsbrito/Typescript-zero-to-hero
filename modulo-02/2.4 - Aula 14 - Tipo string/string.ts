7fabff9625a02fae346bc5b52037e8735366de9b
hudney-fsbrito <hudney.rse@gmail.com>
2024-07-03 17:19:52 -0300

    Modulo 2 - Tipo string

---
+++ modulo-02/2.4 - Aula 14 - Tipo string/string.ts
@@ -0,0 +1,20 @@
+// => Exemplo 01 - Single Quotes
+
+let nomeCompleto: string ='Hudney Brito';
+console.log(nomeCompleto);
+
+
+// => Exemplo 01 - Double Quotes
+
+let funcaoEmpresa: string = "Dev Web em JavaScript"
+console.log(funcaoEmpresa);
+
+// => Exemplo 01 - Single Quotes
+
+let nomeEmpresa: string = 'Microsoft';
+
+let dadosFuncionario: string = `Seja bem-vindo ${nomeCompleto}! Você será ${funcaoEmpresa} na empresa ${nomeEmpresa}`
+
+console.log(dadosFuncionario);
+
+//
